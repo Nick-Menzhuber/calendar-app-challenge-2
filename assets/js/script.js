122a8bd7cb7a0bf56384ed2344ddd5fe25fa8fc6
Nick Menzhuber <nickmenzhuber@Nicks-Mac-mini.local>
2022-12-30 22:53:28 -0600

    resets to starter code keeping just the moment.js bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
     //sets current day and hour
     let currentDay = moment().format("dddd, MMMM Do YYYY");
@@ -7,51 +5,4 @@ $(document).ready(function () {
     displayDate.innerHTML = currentDay;
     let currentHour = moment().format("HH");
   
-    // gives button functionality to clear local storage
-    $("#clearFieldsBtn").click(function (event) {
-      event.preventDefault;
-      $("textarea").val("");
-      localStorage.clear();
-    });
-  
-    //looks at hour from every time slot and compares it to the actual time
-    $(".time-div").each(function () {
-      var timeDiv = $(this).attr("id").split("-")[1];
-      
-      if (currentHour == timeDiv) {
-        $(this).addClass("present-hour");
-        $(this).children(".description").addClass("white-text");
-      } else if (currentHour < timeDiv) {
-        $(this).removeClass("present-hour");
-        $(this).addClass("future-hour");
-      } else if (currentHour > timeDiv) {
-        $(this).removeClass("future-hour");
-        $(this).addClass("past-hour");
-      }
-    });
-  
-    //takes values from time and value divs and saves them to local storage
-    $(".saveBtn").click(function (event) {  
-      event.preventDefault();
-      $(".event-saved-wrapper").show(1000)
-        setInterval(function (){
-            $(".event-saved-wrapper").hide()
-        }, 3000)
-      var value = $(this).siblings(".time-block").val();
-      var time = $(this).parent().attr("id").split("-")[1];
-      localStorage.setItem(time, value);
-    });
- 
-
-  
-    //takes items from local storage and displays them in correct slots
-    $("#hour-09 .time-block").val(localStorage.getItem("09"));
-    $("#hour-10 .time-block").val(localStorage.getItem("10"));
-    $("#hour-11 .time-block").val(localStorage.getItem("11"));
-    $("#hour-12 .time-block").val(localStorage.getItem("12"));
-    $("#hour-13 .time-block").val(localStorage.getItem("13"));
-    $("#hour-14 .time-block").val(localStorage.getItem("14"));
-    $("#hour-15 .time-block").val(localStorage.getItem("15"));
-    $("#hour-16 .time-block").val(localStorage.getItem("16"));
-    $("#hour-17 .time-block").val(localStorage.getItem("17"));
   });
